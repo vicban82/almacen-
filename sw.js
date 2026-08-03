8258a514c8f014a84ccb2d8c140757e54677e761
Victor <39536621+vicban82@users.noreply.github.com>
2026-08-02 22:26:59 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'almacen-v221'; // Incrementa esto cada vez que hagas cambios
+const CACHE_NAME = 'almacen-v222'; // Incrementa esto cada vez que hagas cambios
 const ASSETS = [
     './',
     './index.html',
